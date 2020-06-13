b33760770c217317caedbf6077d427186740e51d
grabania <grabania.work@gmail.com>
2020-06-13 02:02:28 +0100

    Update JS

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-window.addEventListener('keydown', function(e) {
+function playSound(e) {
 	const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
 	if (!audio) return; //stop the function from running all together
 	audio.currentTime = 0; //rewind to the start
 	audio.play();
-});
+}
+
+window.addEventListener('keydown', playSound);
